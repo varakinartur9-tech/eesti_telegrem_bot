f2e43db2de25e35499d4d24a4194bf44a084f3f7
Artur Varakin, Pavel Ogorodnõi, Ivan Vaštšenko <varakinartur9@gmail.com>
2026-02-20 18:15:00 +0200

    Lisasin peamenüü

--- bot.py
+++ bot.py
@@ -6,3 +6,12 @@ from data import WORDS, TEXTS
 from database import cursor, conn
 
 TOKEN = ""
+
+def menu():
+    return ReplyKeyboardMarkup([
+        ["📚 Õpi sõnu","📖 Loe teksti"],
+        ["📝 Test","➕ Lisa sõna"],
+        ["📌 Minu sõnad","📊 Minu statistika"],
+        ["🔄 Reset"]
+    ], resize_keyboard=True)
+    
