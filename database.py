5a83097de82e1da0edc6b0eafa58ee0193bb8cf5
Artur Varakin, Pavel Ogorodnõi, Ivan Vaštšenko <varakinartur9@gmail.com>
2026-04-28 20:20:00 +0300

    Lisatud väikesed kommentaarid

--- database.py
+++ database.py
@@ -1,23 +1,30 @@
 import sqlite3
 
+# Подключаемся к базе данных (если файла нет — он создастся)
+# Ühendume andmebaasiga (kui faili pole, see luuakse)
 conn = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
-cursor.execute("""
+#  Сохраняем результаты теста в базу данных
+#  Salvestame testi tulemused andmebaasi
+cursor.execute("""                  
+
 CREATE TABLE IF NOT EXISTS users(
 user_id INTEGER PRIMARY KEY,
 level TEXT,
 direction TEXT
 )
 """)
-
+# Таблица пользователей (сохраняем уровень и направление)
+# Kasutajate tabel (salvestame taseme ja suuna)
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS my_words(
 user_id INTEGER,
 word TEXT
 )
 """)
-
+# Таблица слов пользователя (слова, которые он добавил)
+# Kasutaja sõnade tabel (tema lisatud sõnad)
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS stats(
 user_id INTEGER,
@@ -26,5 +33,6 @@ correct INTEGER,
 wrong INTEGER
 )
 """)
-
+# Сохраняем изменения в базе данных
+# Salvestame muudatused andmebaasis
 conn.commit()
